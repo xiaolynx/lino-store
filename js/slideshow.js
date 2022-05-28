8290ef190da7792888ae90ceaa63e64855c326df
giaolinh910 <duyrie910@gmail.com>
2022-05-28 14:14:19 +0700

    block

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,6 +1,8 @@
 let slideIndex = 0;
 showSlides(slideIndex);
 
+
+
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -15,14 +17,14 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
